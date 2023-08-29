7e87995c277ea75667ba8aa08df18c684e444012
kevinjohnson123 <celtic_tigger1@hotmail.com>
2023-08-29 18:23:16 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,12 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install 14 && nvm use 14
+
 git clone https://github.com/sourceryltd/M300.git
+cd /home/pi/M300/wifi_ctrl
+npm install
+#npm start
+
 pip install -r /home/pi/M300/requirements.txt
 
 # Update the Boot Splash screen
@@ -17,7 +22,7 @@ sudo mv /usr/share/plymouth/themes/pix/splash.png /usr/share/plymouth/themes/pix
 sudo cp /home/pi/M300/bootimg.png /usr/share/plymouth/themes/pix/splash.png
 
 # Install Mjpg-Streamer
-sudo apt-get install build-essential cmake gcc g++ libjpeg62-turbo-dev libjpeg62-turbo-dev --no-install-recommends xserver-xorg xdotool matchbox-window-manager xautomation ffmpeg hostapd dnsmasq
+sudo apt-get install build-essential cmake gcc g++ libjpeg62-turbo-dev libjpeg62-turbo-dev --no-install-recommends xserver-xorg xdotool matchbox-window-manager xautomation ffmpeg hostapd dnsmasq python2
 git clone https://github.com/jacksonliam/mjpg-streamer.git
 cd mjpg-streamer
 cd mjpg-streamer-experimental

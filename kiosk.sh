09a5aac69d128eafec3c8533e85a7859f07ae7d5
kevinjohnson123 <celtic_tigger1@hotmail.com>
2023-08-31 14:43:36 -0400

    Update kiosk.sh

--- kiosk.sh
+++ kiosk.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # Start the NodeJS  Wifi Control API Server
-node /home/pi/M300/wifi_ctrl/index.js &
+#node /home/pi/M300/wifi_ctrl/index.js &
 
 # Start the API Server
 python /home/pi/M300/server.py &
-cd /media/usb
 
 # Start the File Server 
+cd /media/usb
 python2 /home/pi/M300/file_server.py 9990 &
-cd /home/pi/M300/ui
 
 # Serve the React App on Port 80
+cd /home/pi/M300/ui
 sudo python -m http.server 80 &
 
 DISPLAY=:0.0 xset s noblank

40dbc61061deef20b3a08d983dac4059185969b8
kevinjohnson123 <celtic_tigger1@hotmail.com>
2023-08-30 22:24:06 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 # M300-3D_Printer
 
+# Expand Filesystem to use entire disk
+sudo raspi-config
 
 sudo mkdir /media/usb/
 sudo apt-get update
 sudo apt-get install build-essential cmake gcc g++ libjpeg62-turbo-dev libjpeg62-turbo-dev --no-install-recommends xserver-xorg xinit xdotool matchbox-window-manager xautomation ffmpeg hostapd dnsmasq python2 python3-pip git cmake plymouth plymouth-themes pix-plym-splash chromium-browser
-sudo dpkg-reconfigure x11-common
 sudo apt-get upgrade
 
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
@@ -22,7 +23,6 @@ cd ..
 pip install -r /home/pi/M300/requirements.txt
 
 # Update the Boot Splash screen
-sudo mv /usr/share/plymouth/themes/pix/splash.png /usr/share/plymouth/themes/pix/splash.png.bak
 sudo cp /home/pi/M300/bootimg.png /usr/share/plymouth/themes/pix/splash.png
 
 # Install Mjpg-Streamer
@@ -32,11 +32,16 @@ cd mjpg-streamer-experimental
 make
 sudo make install
 
-cat .bashrc >> nvm use 14
-cat /home/pi/.bashrc >> xinit /home/pi/M300/kiosk.sh -- vt$(fgconsole)
+echo "nvm use 14" >> /home/pi/.bashrc
+echo "xinit /home/pi/M300/kiosk.sh -- vt$(fgconsole)" >> /home/pi/.bashrc
 
 # Copy System services
 sudo cp /home/pi/M300/startup_services/autohotspot.service /etc/systemd/system
 sudo cp /home/pi/M300/startup_services/splashscreen.service /etc/systemd/system
 sudo systemctl enable autohotspot.service
 sudo systemctl enable splashscreen.service
+
+# Use raspi-config to set the login mode to  console + autologin
+sudo raspi-config
+
+#

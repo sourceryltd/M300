1e125d75c2bc45dd2f7f299d04581c2e99d73fb0
kevinjohnson123 <celtic_tigger1@hotmail.com>
2023-09-04 21:07:36 -0400

    Add files via upload

---
+++ www/html/touch-ui/precache-manifest.6d6921b0e5972dd91130282ce578b596.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "783da5955075478445bded154e3dfdfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "969b11b14556304e6e4f",
+    "url": "/static/css/2.7bc29695.chunk.css"
+  },
+  {
+    "revision": "46453bc09fa7b05ad7cc",
+    "url": "/static/css/main.745d7fbd.chunk.css"
+  },
+  {
+    "revision": "969b11b14556304e6e4f",
+    "url": "/static/js/2.827e2ec1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.827e2ec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46453bc09fa7b05ad7cc",
+    "url": "/static/js/main.75d20b8e.chunk.js"
+  },
+  {
+    "revision": "79e855487fa96201629887902955fb64",
+    "url": "/static/js/main.75d20b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d4948f5a71a0477da3b",
+    "url": "/static/js/runtime-main.62bf03f1.js"
+  },
+  {
+    "revision": "01cf77d47e997eaaf04952876abad736",
+    "url": "/static/media/mm-print.01cf77d4.svg"
+  }
+]);
